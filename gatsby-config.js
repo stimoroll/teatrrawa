4474a53037deec6556db45e04187db6f8d76f39d
Poplawski, Krzysztof <kpoplawski@live.com>
2021-10-29 22:17:24 +0200

    env in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,18 @@ let env = process.env.NODE_ENV || 'development';
 // require("dotenv").config({
   // path: `.env.${process.env.NODE_ENV}`,
 // })
-
+/*
 const dotenv = require('dotenv');
 const envConfig = 
 dotenv.parse(fs.readFileSync(`.env.${process.env.NODE_ENV}`));
 for (var k in envConfig) {
   process.env[k] = envConfig[k];
-}
+}*/
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"]=0
+process.env["CLOUDINARY_CLOUD_NAME"]='teatrrawa'
+process.env["CLOUDINARY_API_KEY"]='683758242113594'
+process.env["CLOUDINARY_API_SECRET"]='0_TXlU3wvOruUNdEOKbTW76A4gg'
+process.env["CLOUDINARY_URL"]='cloudinary://683758242113594:0_TXlU3wvOruUNdEOKbTW76A4gg@teatrrawa'
 
 module.exports = {
   pathPrefix: "/teatrrawa", //put here github
