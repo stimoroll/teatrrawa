7675f37db196b2bc5bbe73165c0dbfab7576f6c8
stimoroll <stimoroll@users.noreply.github.com>
2021-11-08 16:30:16 +0000

    deployed via Gatsby Publish Action 🎩 for b35d9b64d3c45ad741de454adfb258227830f9a2

---
+++ component---src-pages-404-js-bd01ced1dd091103ac8e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktrawa_wp=self.webpackChunktrawa_wp||[]).push([[883],{9616:function(e,t,n){n.r(t);var u=n(7294),a=n(1404),l=n(4910);t.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-bd01ced1dd091103ac8e.js.map
---
+++ component---src-pages-avbout-js-0b37197dcd4f3d687002.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktrawa_wp=self.webpackChunktrawa_wp||[]).push([[551],{4899:function(e,t,n){n.r(t);var a=n(7294);t.default=function(){return a.createElement("main",null,a.createElement("h1",null,"About the Author"),a.createElement("p",null,"Welcome to my Gatsby site."))}}}]);
+//# sourceMappingURL=component---src-pages-avbout-js-0b37197dcd4f3d687002.js.map
