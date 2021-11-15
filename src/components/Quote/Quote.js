4703bb8b9f07df4a29b47b5189e61500d618b99b
Poplawski, Krzysztof <kpoplawski@live.com>
2021-11-15 15:40:09 +0100

    temp changes

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -6,15 +6,15 @@ import { Wrapper, Content } from "./Quote.styles"
 const Quote = () => {
   const data = useQuoteQuery()
 
-  const header = data.wpPage.ACF_HomePage.citat1Text
-  const text = data.wpPage.ACF_HomePage.citat1Author
+  // const header = data.wpPage.ACF_HomePage.citat1Text
+  // const text = data.wpPage.ACF_HomePage.citat1Author
 
   return (
     <Wrapper>
       <Content>
-        <img src={QuoteImg} alt="quote" />
-        <h6>{header}</h6>
-        <p>{text}</p>
+        {/* <img src={QuoteImg} alt="quote" /> */}
+        {/* <h6>{header}</h6> */}
+        {/* <p>{text}</p> */}
       </Content>
     </Wrapper>
   )
